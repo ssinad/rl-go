dc4b7fc61a64d922b4699c591fa7135ad98ff1cf
ssd352 <27953063+ssd352@users.noreply.github.com>
2018-04-28 23:41:29 -0600

    Version 0.1a

--- exp/exp.go
+++ exp/exp.go
@@ -1,37 +1,45 @@
 package main
 
-import(
-	"rl/glue"
+import (
 	"fmt"
+	"rl/glue"
 )
 
-const max_steps = 10000
+// const max_steps = 10000
 
 var (
-	num_episodes = 8000
-	done chan bool = make(chan bool)
+	num_episodes           = 8000
+	max_steps              = 10000
+	done         chan bool = make(chan bool)
 )
-func single_run(run int){
+
+func single_run(run int) {
 	fmt.Printf("run number %d\n\n", (run + 1))
 	rl.Init()
 	// fmt.Println()
-	
-	for episode := 0; episode < num_episodes; episode++{
+
+	for episode := 0; episode < num_episodes; episode++ {
 		rl.Episode(max_steps)
 	}
 	rl.Cleanup()
 	done <- true
 }
 
-func main(){
-	
-	num_runs := 10
-	
+func main() {
 
-	for run := 0; run < num_runs; run++{
+	num_runs := 10
+	runs := make([]float64, num_runs)
+	runs[0] = 0.1
+	runs[1] = 0.2
+	runs[2] = 0.3
+	for _, value := range runs {
+		fmt.Println(value)
+	}
+
+	for run := 0; run < num_runs; run++ {
 		go single_run(run)
 	}
-	for run := 0; run < num_runs; run++{
-		<- done
+	for run := 0; run < num_runs; run++ {
+		<-done
 	}
 }
